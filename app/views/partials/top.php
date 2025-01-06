896b6f84e92c7d4cf9d84cec7e2847860ff14ffd
ElFirdaous28 <elmokhtarifirdaous@gmail.com>
2025-01-06 22:57:17 +0100

    client initial pages

--- app/views/partials/top.php
+++ app/views/partials/top.php
@@ -4,8 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Administration - Dashboard</title>
-        <script
-            src="https://cdnjs.cloudflare.com/ajax/libs/lucide/0.263.1/umd/lucide.min.js"></script>
+
         <script src="https://cdn.tailwindcss.com"></script>
         <script src="https://unpkg.com/lucide@latest/dist/umd/lucide.js"></script>
 
