62c944bd6225fe477b47511aac07fe97cbf89644
ElFirdaous28 <elmokhtarifirdaous@gmail.com>
2025-01-06 22:05:01 +0100

    admin initial pages

--- public/index.php
+++ public/index.php
@@ -6,6 +6,7 @@ require_once '../core/Router.php';
 require_once '../core/Route.php';
 
 require_once '../app/controllers/AuthController.php';
+require_once '../app/controllers/AdminController.php';
 // require_once '../app/config/db.php';
 
 
@@ -18,6 +19,11 @@ Route::setRouter($router);
 // Define routes
 // auth routes 
 Route::get('/register', [AuthController::class, 'showRegister']);
+Route::get('/admin/dashboard', [AdminController::class, 'adminDashboard']);
+Route::get('/admin/clients', [AdminController::class, 'clientsPage']);
+Route::get('/admin/comptes', [AdminController::class, 'comptesPage']);
+Route::get('/admin/transactions', [AdminController::class, 'transactionsPage']);
+
 // Route::post('/register', [AuthController::class, 'handleRegister']);
 // Route::get('/login', [AuthController::class, 'showleLogin']);
 // Route::post('/login', [AuthController::class, 'handleLogin']);
