b6ff7db41c0912b22a181d959922fca1004680f8
ElFirdaous28 <elmokhtarifirdaous@gmail.com>
2025-01-07 17:01:29 +0100

    GDCB-10 add depot

--- app/models/Account.php
+++ app/models/Account.php
@@ -8,16 +8,46 @@ class Account extends DataBase
     }
 
     // method to get client accounts
-    public function clientAccounts($user_is)
+    public function clientAccounts($user_id)
     {
         try {
             $accountsStatement = $this->conn->prepare("SELECT * FROM accounts WHERE user_id = ?");
-            $accountsStatement->execute([$user_is]);
+            $accountsStatement->execute([$user_id]);
 
             $accounts = $accountsStatement->fetchAll(PDO::FETCH_ASSOC);
             return $accounts;
         } catch (PDOException $e) {
             echo "Error finding user accounts: " . $e->getMessage();
+        }
+    }
+
+
+    // methode to get amount 
+    public function getAmount($account_id)
+    {
+        try {
+            $getAmountStatement = $this->conn->prepare("SELECT balance FROM accounts WHERE account_id = ?");
+            $getAmountStatement->execute([$account_id]);
+
+            $amount = $getAmountStatement->fetchColumn();;
+            return $amount;
+        } catch (PDOException $e) {
+            echo "Error finding user accounts: " . $e->getMessage();
+        }
+    }
+
+    // methode to update the amount
+    public function addAmount($account_id,$transaction_amount)
+    {
+        echo "add amount called";
+        $oldAmount= $this->getAmount($account_id);
+        $newAmount=(int)$oldAmount+(int)$transaction_amount;
+
+        try {
+            $updateAmountStatement = $this->conn->prepare("UPDATE accounts SET balance=? WHERE account_id=?");
+            $updateAmountStatement->execute([$newAmount,$account_id]);
+        } catch (PDOException $e) {
+            echo "Error updating the amount: " . $e->getMessage();
         }
     }
 }
