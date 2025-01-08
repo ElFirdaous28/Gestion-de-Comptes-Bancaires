781cf0fcf67a4306aa1cb8437977fc02a6b7df27
soaud arziki <arziki.souad@gmail.com>
2025-01-08 17:00:35 +0100

    GDCB-23 affichage des clinets pour l'administrateur

--- app/models/User.php
+++ app/models/User.php
@@ -6,6 +6,21 @@ class User extends DataBase{
         parent::__construct();
     }
 
+    public function login($userData){
+        
+        try {
+            $result = $this->conn->prepare("SELECT * FROM users WHERE email=?");
+            $result->execute([$userData[0]]);
+            $user = $result->fetch(PDO::FETCH_ASSOC);
+            
+            if($user && password_verify($userData[1], $user["password"])){
+                return  $user ;
+            }
+        } catch (PDOException $e) {
+            echo "Error in login function: " . $e->getMessage();
+        }
+    }
+
     public function addUser($fullname, $email,$hashed_password,$role){
         try{
             $stmt = $this->conn->prepare("INSERT INTO users (full_name, email, password,role) VALUES(?,?,?,?)");
@@ -13,22 +28,30 @@ class User extends DataBase{
         }catch (PDOException $e) {
             echo "Error in add user: " . $e->getMessage();
         }
-
     }
 
-    public function login($userData){
-    
-        try {
-            $result = $this->conn->prepare("SELECT * FROM users WHERE email=?");
-            $result->execute([$userData[0]]);
-            $user = $result->fetch(PDO::FETCH_ASSOC);
-    
-            if($user && password_verify($userData[1], $user["password"])){
-               return  $user ;
-            }
-        } catch (PDOException $e) {
-            echo "Error in login function: " . $e->getMessage();
+    public function showUsers(){
+        try{
+            $showUsersQuery = $this->conn->prepare("SELECT users.*,COUNT(accounts.user_id) as accounts_number, accounts.account_type 
+                                            FROM users
+                                            LEFT JOIN accounts ON users.user_id = accounts.user_id
+                                            GROUP BY users.user_id, accounts.account_type;");
+            $showUsersQuery->execute();
+
+            $users = $showUsersQuery->fetchAll(PDO::FETCH_ASSOC);
+            return $users;
+        }catch (PDOException $e) {
+            echo "Error in show user: " . $e->getMessage();
         }
     }
-}
+
+    public function updateUser($fullname,$user_id){
+        try{
+            $updateUserQuery = $this->conn->prepare("UPDATE users SET full_name = ?, email = ?, WHERE user_id = ?");
+            $updateUserQuery->execute([$fullname,$user_id]);
+        }catch(PDOException $e){
+            echo "Error in update user: " . $e->getMessage();
+        }
+    }
+} 
 ?>
