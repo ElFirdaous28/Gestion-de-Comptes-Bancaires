8581a992f5ad1a65641354398f29cf1e294049df
ElFirdaous28 <elmokhtarifirdaous@gmail.com>
2025-01-08 21:57:27 +0100

    GDCB-1 database modification

--- bank_management.sql
+++ bank_management.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 06, 2025 at 02:23 PM
+-- Generation Time: Jan 08, 2025 at 08:55 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -24,72 +24,105 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `account`
+-- Table structure for table `accounts`
 --
 
-CREATE TABLE `account` (
-  `id_account` int NOT NULL,
+CREATE TABLE `accounts` (
+  `account_id` varchar(100) NOT NULL,
   `user_id` int DEFAULT NULL,
   `account_type` enum('courant','epargne') DEFAULT NULL,
   `balance` decimal(10,2) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `account_status` enum('active','blocked') DEFAULT 'active',
+  `plafond_retrait_jour` decimal(10,2) DEFAULT '1000.00',
+  `decouvert_autorise` decimal(10,2) DEFAULT '500.00',
+  `frais_tenue` decimal(10,2) DEFAULT '10.00'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `transaction`
+-- Table structure for table `beneficiaries`
 --
 
-CREATE TABLE `transaction` (
+CREATE TABLE `beneficiaries` (
+  `user_id` int NOT NULL,
+  `beneficiary_account_id` varchar(100) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `beneficiary_name` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transactions`
+--
+
+CREATE TABLE `transactions` (
   `id_transaction` int NOT NULL,
-  `account_id` int NOT NULL,
+  `account_id` varchar(100) NOT NULL,
   `transaction_type` enum('depot','retrait','transfert') DEFAULT NULL,
   `amount` decimal(10,2) DEFAULT NULL,
-  `beneficiary_account_id` int DEFAULT NULL,
+  `beneficiary_account_id` varchar(100) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Table structure for table `users`
 --
 
-CREATE TABLE `user` (
+CREATE TABLE `users` (
   `user_id` int NOT NULL,
   `full_name` varchar(100) DEFAULT NULL,
   `email` varchar(225) DEFAULT NULL,
   `password` varchar(225) DEFAULT NULL,
   `profile_pic` varchar(225) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `role` enum('admin','client') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `full_name`, `email`, `password`, `profile_pic`, `created_at`, `updated_at`, `role`) VALUES
+(1, 'admin', 'admin@gmail.com', '$2y$10$LJjPWaxEea7Tl33.t2NsYOZY4CZ1qOUEHbmstzRJAqdMMfU3w47we', NULL, '2025-01-08 11:01:52', '2025-01-08 19:56:31', 'admin'),
+(5, 'client1', 'client1@gmail.com', '$2y$10$AHjdhcWR/eEDsICVIsyTtu0RAUi/D9d9PTNvqhYRLRuZINb4KMOFq', NULL, '2025-01-08 20:53:50', '2025-01-08 20:53:50', 'client');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `account`
+-- Indexes for table `accounts`
 --
-ALTER TABLE `account`
-  ADD PRIMARY KEY (`id_account`),
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`account_id`),
   ADD KEY `account_ibfk_2` (`user_id`);
 
 --
--- Indexes for table `transaction`
+-- Indexes for table `beneficiaries`
 --
-ALTER TABLE `transaction`
+ALTER TABLE `beneficiaries`
+  ADD PRIMARY KEY (`user_id`,`beneficiary_account_id`),
+  ADD KEY `beneficiaries_ibfk_2` (`beneficiary_account_id`);
+
+--
+-- Indexes for table `transactions`
+--
+ALTER TABLE `transactions`
   ADD PRIMARY KEY (`id_transaction`),
   ADD KEY `account_id` (`account_id`),
   ADD KEY `beneficiary_account_id` (`beneficiary_account_id`);
 
 --
--- Indexes for table `user`
+-- Indexes for table `users`
 --
-ALTER TABLE `user`
+ALTER TABLE `users`
   ADD PRIMARY KEY (`user_id`);
 
 --
@@ -97,39 +130,40 @@ ALTER TABLE `user`
 --
 
 --
--- AUTO_INCREMENT for table `account`
+-- AUTO_INCREMENT for table `transactions`
 --
-ALTER TABLE `account`
-  MODIFY `id_account` int NOT NULL AUTO_INCREMENT;
+ALTER TABLE `transactions`
+  MODIFY `id_transaction` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `transaction`
+-- AUTO_INCREMENT for table `users`
 --
-ALTER TABLE `transaction`
-  MODIFY `id_transaction` int NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `user_id` int NOT NULL AUTO_INCREMENT;
+ALTER TABLE `users`
+  MODIFY `user_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `account`
+-- Constraints for table `accounts`
 --
-ALTER TABLE `account`
-  ADD CONSTRAINT `account_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `accounts`
+  ADD CONSTRAINT `accounts_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `beneficiaries_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
 
 --
--- Constraints for table `transaction`
+-- Constraints for table `beneficiaries`
 --
-ALTER TABLE `transaction`
-  ADD CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `account` (`id_account`),
-  ADD CONSTRAINT `transaction_ibfk_2` FOREIGN KEY (`beneficiary_account_id`) REFERENCES `account` (`id_account`);
+ALTER TABLE `beneficiaries`
+  ADD CONSTRAINT `beneficiaries_ibfk_2` FOREIGN KEY (`beneficiary_account_id`) REFERENCES `accounts` (`account_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`account_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `transactions_ibfk_2` FOREIGN KEY (`beneficiary_account_id`) REFERENCES `accounts` (`account_id`) ON DELETE SET NULL;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
