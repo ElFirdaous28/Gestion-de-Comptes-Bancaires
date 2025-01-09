d7dcc2ba979e5a5a1fc60d7bf9388be0afd551be
soaud arziki <arziki.souad@gmail.com>
2025-01-09 14:04:40 +0100

    GDCB-21 GDCB-22 modification et suppression d'un utilisateur

---
+++ app/models/Account.php
@@ -0,0 +1,23 @@
+<?php 
+require_once (__DIR__.'/../config/database.php');
+
+class Account extends DataBase{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+public function addAcount($account_id, $user_id, $account_type, $balance,$plafond_retrait_jour,$decouvert_autorise){
+    echo "called";
+    try{
+        $stmt = $this->conn->prepare("INSERT INTO users (account_id, user_id, account_type, balance,plafond_retrait_jour,decouvert_autorise) VALUES(?,?,?,?,?,?)");
+        $stmt->execute([$account_id, $user_id, $account_type, $balance,$plafond_retrait_jour,$decouvert_autorise]);
+        echo "done";die();
+    }catch (PDOException $e) {
+        echo "Error in add acount: " . $e->getMessage();
+    }
+}
+}
+
+
+?>
